14d9e276462bff50f77c23d793c8d0b719ea1989
Miguel Angel Teletor <miguelangel@MacBook-Air-de-Miguel.local>
2020-04-16 22:19:15 +0200

    right now my first commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/* Route*/
+
+//list Routes
+
+
+/*
++get/lists
+*purpose: Get all lists
+*/
+
+app.get('/lists', (req, res) => {
+      // we want to return an array of all lthe lists of dataase
+});
+/** post /lists
+ * purpose: create a lists
+ * 
+*/
+
+app.post('/lists', (req, res) =>{
+//HERE I CREATE A NEW LIST and return the lists which include the Id
+
+
+});
+/**
+ * 
+*/
+app.patch('/lists/:id', (req, res) =>{
+//to update the list here with the id on the url
+
+})
+
+
+
+
+app.use(express.json());
+app.listen(PORT,()=>console.log('server running on port '+PORT))
